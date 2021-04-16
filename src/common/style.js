5d8cb9c5ac642584a5353e6e1eca6d1667497a1a
Vy <vyntz@necsv-hcm.com>
2021-04-16 17:39:28 +0700

    stylying

--- src/common/style.js
+++ src/common/style.js
@@ -2,33 +2,50 @@ import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
     inputfield: {
-        height: 25,
-        width: '70%',
+        height: 35,
+        width: '50%',
+        paddingLeft: 5,
+        backgroundColor: '#B6B0BD',
+        borderRadius: 6
     },
     searchbutton: {
         width: '30%',
-        height: 25,
-        paddingRight: 10
-
+        marginTop: 0,
+        height: 35,
+        paddingRight: 0,
+        backgroundColor: '#d9egf3', 
+        borderRadius: 6
     },
     containerInput: {
         flexDirection: 'row',
         width: '100%',
-        paddingLeft: 50,
         flex: 1,
-        backgroundColor: 'red',
-        height: 30
+        marginTop: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
+        backgroundColor: '#EFEAE0'
     },
     container: {
-        paddingTop: 30,
+        width: '100%',
         justifyContent: 'flex-start',
         alignItems: 'center',
         flexDirection: 'column',
-        flex: 10
+        flex: 20,
+        backgroundColor: '#999891'
     },
     listContainer: {
-        marginTop: 0,
+        marginTop: 30,
+        margin: 5,
         width: '100%',
-        flex: 9
-      }
+        flex: 19,
+        backgroundColor: '#EFEAE0'
+      },
+    itemContainer: {
+        backgroundColor: '#DAB6C3',
+        height: 30,
+        margin: 10,
+        borderRadius: 6,
+        paddingLeft: 5
+    }
 })
--- src/view/main/mainview.js
+++ src/view/main/mainview.js
@@ -1,60 +1,53 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { View, Text, TextInput, FlatList, Button} from 'react-native'
 import stylesheet from '../../common/style.js'
 
 
-const data = require('../../data/data.json')
+const collections = require('../../data/data.json')
 
-export default class MainView extends Component{
-    state = {
-        searchName: "",
-        advisors: [
-            {
-                name: "A"
-            },
-            {
-                name: "b"
-            }
-        ]
-    }
-
-    nameForSearch = (name) =>{
-        this.setState({
-            searchName: name
-        })
-
-    }
-
-    searchForName = () =>{
-
-    }
-
-    render(){
-        return(
-        <View style={stylesheet.container}> 
-            <View style={stylesheet.containerInput}>
-                <TextInput style={stylesheet.inputfield}
-                placeholder="text input"
-                value = {this.state.searchName}
-                onChangeText = {this.nameForSearch}
-                />
-                <View style={stylesheet.searchbutton}>
-                    <Button onClick={this.searchForName} 
-                    title="Search"/>
-                </View>
-                
-            </View>
-            <View style = {stylesheet.listContainer}> 
-                <FlatList
-                data = {data.data.advisorProfileCollection.items}
-                keyExtractor = {item => item.displayName}
-                renderItem = {items => (
-                    <View>
-                        <Text>{items.item.displayName}</Text>
-                    </View>
-                )}/>
-            </View>
+const MainView = () => {
+    const [searchValue, setSearchValue] = useState("");
+  
+    const [advisorList, setAdvisorList] = useState([
+      {
+        name: "A",
+      },
+      {
+        name: "b",
+      },
+    ]);
+  
+    const handleSearchChange = (value) => setSearchValue(value);
+  
+    const handleSearchClick = () => {};
+  
+    return (
+      <View style={stylesheet.container}>
+        <View style={stylesheet.containerInput}>
+          <TextInput
+            style={stylesheet.inputfield}
+            placeholder="Search name ..."
+            placeholderTextColor="#fff"
+            value={searchValue}
+            onChangeText={handleSearchChange}
+          />
+          <View style={stylesheet.searchbutton}>
+            <Button onClick={handleSearchClick} title="Search" color="#42b883" />
+          </View>
         </View>
-        )
-    }
-}
+        <View style={stylesheet.listContainer}>
+          <FlatList
+            data={collections.data.advisorProfileCollection.items}
+            keyExtractor={(item) => item.sys.id}
+            renderItem={(items) => (
+              <View style={stylesheet.itemContainer}>
+                <Text style={stylesheet.listItem}>{items.item.displayName}</Text>
+              </View>
+            )}
+          />
+        </View>
+      </View>
+    );
+  };
+  
+  export default MainView;
